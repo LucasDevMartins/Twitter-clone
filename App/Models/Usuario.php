eda74ea81878eb90f944c175f839feff3b8facb4
Lucas Martins <lucas.rodriguesmartins@hotmail.com>
2024-09-03 11:42:48 -0300

    Colocando funcionalidades nos usuarios e criando e selecionando novas tabelas

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use MF\Model\Model;
+use PDO;
 
 class Usuario extends Model {
 
@@ -60,6 +61,66 @@ class Usuario extends Model {
 		$stmt->execute();
 
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	//autenticação de um usuário
+	public function autenticar() {
+
+		$query = "select id, nome, email from usuarios where email = :email and senha = :senha";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':email', $this->__get('email'));
+		$stmt->bindValue(':senha', $this->__get('senha'));
+		$stmt->execute();
+
+		$usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if (!empty($usuario['id']) && !empty($usuario['nome'] != '')) {
+			$this->__set('id', $usuario['id']);
+			$this->__set('nome', $usuario['nome']);
+		}
+
+		return $this;
+	}
+
+	public function getAll(){
+		$query = "
+		select 
+			u.id, u.nome, u.email,(
+				select count(*) from usuarios_seguidores as us where us.id_usuario = :id_usuario and us.id_usuario_seguindo = u.id
+			) as seguindo_sn 
+		from 
+			usuarios as u
+		where 
+			u.nome like :nome and u.id != :id_usuario
+			";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':nome', '%'.$this->__get('nome').'%');
+		$stmt->bindValue(':id_usuario', $this->__get('id'));
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function seguirUsuario($id_usuario_seguindo) {
+		$query = "insert into usuarios_seguidores(id_usuario, id_usuario_seguindo)values(:id_usuario, :id_usuario_seguindo)";
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':id_usuario', $this->__get('id'));
+		$stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+		$stmt->execute();
+
+		return true;
+	}
+
+	public function deixarSeguirUsuario($id_usuario_seguindo) {
+		$query = "delete from usuarios_seguidores where id_usuario = :id_usuario and id_usuario_seguindo = :id_usuario_seguindo";
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':id_usuario', $this->__get('id'));
+		$stmt->bindValue(':id_usuario_seguindo', $id_usuario_seguindo);
+		$stmt->execute();
+
+		return true;
 	}
 }
 
--- App/Route.php
+++ App/Route.php
@@ -26,6 +26,43 @@ class Route extends Bootstrap {
 			'action' => 'registrar'
 		);
 
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+
+		
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
+		);
+		
+		$routes['tweet'] = array(
+			'route' => '/tweet',
+			'controller' => 'AppController',
+			'action' => 'tweet'
+		);
+		
+		$routes['quem_seguir'] = array(
+			'route' => '/quem_seguir',
+			'controller' => 'AppController',
+			'action' => 'quemSeguir'
+		);
+
+		$routes['acao'] = array(
+			'route' => '/acao',
+			'controller' => 'AppController',
+			'action' => 'acao'
+		);
+
 		$this->setRoutes($routes);
 	}
 
